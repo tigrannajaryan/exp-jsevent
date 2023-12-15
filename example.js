c5bff475c00855ae67d4528ffc9acd111ac29cf6
Tigran Najaryan <tigran@najaryan.net>
2023-12-15 14:49:02 -0500

    Use typescript

---
+++ example.js
@@ -0,0 +1,100 @@
+function createFlatEvent(name, fieldCount) {
+    var e = { "event.name": name };
+    for (var i = 0; i < fieldCount; i++) {
+        e[name + ".attr" + i] = i;
+    }
+    return e;
+}
+function createNestedEvent(name, fieldCount) {
+    var e = { "event.name": name };
+    var payload = {};
+    for (var i = 0; i < fieldCount; i++) {
+        payload["attr" + i] = i;
+    }
+    e["event.payload"] = payload;
+    return e;
+}
+function generateEvents(generator) {
+    recordEvent(generator("browser.page_view", 4));
+    recordEvent(generator("browser.page_navigation_timing", 21));
+    recordEvent(generator("browser.web_vitals", 15));
+    for (var i = 0; i < 6; i++) {
+        recordEvent(generator("browser.http_request", 6));
+    }
+    for (var i = 0; i < 112; i++) {
+        recordEvent(generator("browser.resource_timing", 20));
+    }
+    exportBatch();
+}
+function testNestedEvents() {
+    console.log("Generating nested events");
+    generateEvents(createNestedEvent);
+}
+function testFlatEvents() {
+    console.log("Generating flattened events");
+    generateEvents(createFlatEvent);
+}
+function onLoad() {
+    testFlatEvents();
+    testNestedEvents();
+}
+var batch = [];
+function recordEvent(e) {
+    batch.push(e);
+}
+function exportBatch() {
+    console.log("Exporting", batch.length, "events...");
+    var start = performance.now();
+    var dict = { strings: {}, len: 0 };
+    var encodedBatch = [];
+    var origBatch = [];
+    for (var _i = 0, batch_1 = batch; _i < batch_1.length; _i++) {
+        var record = batch_1[_i];
+        var encodedRecord = encodeObject(record, dict);
+        encodedBatch.push(encodedRecord);
+        origBatch.push(record);
+    }
+    var exportData = { r: encodedBatch, d: encodeDict(dict) };
+    //console.log("Unencoded export data", batch)
+    //console.log("Encoded export data", exportData)
+    var jsOrig = JSON.stringify(origBatch);
+    var jsEncoded = JSON.stringify(exportData);
+    batch = [];
+    var end = performance.now();
+    console.log("Original JSON len:", jsOrig.length, "bytes,", "Encoded JSON len:", jsEncoded.length, "bytes");
+    console.log("Encoding time: ".concat(end - start, " ms"));
+}
+function encodeObject(obj, dict) {
+    var encoded = {};
+    for (var key in obj) {
+        var keyRef = encodeVal(key, dict);
+        var val = obj[key];
+        if (typeof val == "object") {
+            val = encodeObject(val, dict);
+        }
+        encoded[keyRef] = val;
+    }
+    return encoded;
+}
+function encodeVal(val, dict) {
+    var ref;
+    if (val in dict.strings) {
+        ref = dict.strings[val];
+    }
+    else {
+        dict.len++;
+        ref = dict.len;
+        dict.strings[val] = ref;
+    }
+    return ref;
+}
+function encodeDict(dict) {
+    var d = [];
+    for (var key in dict.strings) {
+        var ref = dict.strings[key];
+        d[ref] = key;
+    }
+    return d;
+}
+onLoad();
+//# sourceMappingURL=example.js.map
